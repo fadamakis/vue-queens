135ed83ec8d0e8adc8f3ea23ab3799f43bbdc738
Fotis Adamakis <fadamakis@gmail.com>
2024-06-25 13:54:42 +0200

    deploy

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/play',
+      path: '/',
       component: () => import('../views/GamePage.vue')
     }
   ]
